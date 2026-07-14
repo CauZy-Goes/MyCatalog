40a309ebbb5bb330b345f21545da164916553885
CauZy-Goes <cauafariasdev@gmail.com>
2026-07-14 09:59:34 -0300

    feat: create CategoryItem model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from models.entity import Entity
 from models.category import Category
+from models.category_item import CategoryItem
 
 entity = Entity(
     name="Example Entity",
@@ -13,6 +14,15 @@ print(entity)
 category = Category(
     name="Example Category ",
     entity_id=1,
+    entity=entity,
 )
 
 print(category)
+
+categoryItem = CategoryItem(
+    name="Example Category Item",
+    category_id=1,
+    category=category,
+)
+
+print(categoryItem)
