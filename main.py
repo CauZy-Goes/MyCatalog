1d54427ec465d06a144c97f0f8d962ac7a584fbe
CauZy-Goes <cauafariasdev@gmail.com>
2026-07-15 15:52:12 -0300

    feat: update main

--- main.py
+++ main.py
@@ -1,75 +1,16 @@
-from datetime import datetime
-from models.entity import Entity
-from models.category import Category
-from models.category_item import CategoryItem
-
-from services.entity_service import EntityService
+from fastapi import FastAPI
 
 from config.database.database import init_db
 
-if __name__ == "__main__":
-    init_db() 
+from controllers.entity_controller import router as entity_router
 
-entityService = EntityService()
-
-# print("All entities:")
-# entities = entityService.find_all()
-# for entity in entities:
-#     print(entity)
-
-print(entityService.find_by_name("Co"))
-
-entity = Entity(
-    name="Coco Bambu",
-    address="Shooping Paralela",
-    created_at=datetime(1999, 2, 13),
-    phoneNumber=987654321,
-    email="coco@gmail.com",
-    description="Restaurante de frutos do mar"
+app = FastAPI(
+    title="My Catalog API",
+    version="1.0.0"
 )
 
-# entityService = EntityService()
+init_db()
 
-# entityService.create_entity(
-#     Entity(
-#         name="Construação Aqui",
-#         address="Lauro De freitas",
-#         created_at=datetime(2023, 12, 23),
-#         phoneNumber=123456789,
-#         email="contruaaqui@gmail.com",
-#         description="Empresa de construção civil"
-#     )
-# )
+app.include_router(entity_router)
 
-# entityService.create_entity(entity)
-
-# print(entityService.delete_entity(10))
-
-
-# targetEntity = entityService.find_by_id(9)
-# targetEntity.name = "Coco Caju"
-
-# print(targetEntity)
-
-# print(entityService.update_entity(targetEntity.id, name=targetEntity.name))
-
-# print(targetEntity)
-
-
-#print(entity)
-
-# category = Category(
-#     name="Example Category ",
-#     entity_id=1,
-#     entity=entity,
-# )
-
-# print(category)
-
-# categoryItem = CategoryItem(
-#     name="Example Category Item",
-#     category_id=1,
-#     category=category,
-# )
-
-# print(categoryItem)
+# uvicorn main:app --reload
